f28ae4f93844d8be1bf8d0fe721bfa153269d82c
swarnim3103 <143250446+swarnim3103@users.noreply.github.com>
2025-02-14 03:59:39 +0530

    Changes to be committed: 	new file:   1d_peak.cpp 	new file:   2d_peak.cpp

---
+++ 1d_peak.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int peak(int a[], int x,int low, int high) {
+  int mid = (low + high) / 2;
+  if (mid < x - 1 && a[mid] < a[mid + 1]) {
+    return peak(a,x, mid + 1, high);
+  } else if (mid > 0 && a[mid] < a[mid - 1]) {
+    return peak(a,x, low, mid - 1);
+  } else {
+    return mid;
+  }
+}
+
+int main() {
+  int n,a[100];
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  int y= a[peak(a, 0, n - 1, n)];
+cout<<"peak is "<<y;
+  return 0;
+}
