14bb2b53dd9c28b235c590db75832117c54e00d0
swarnim3103 <143250446+swarnim3103@users.noreply.github.com>
2025-04-26 23:59:42 +0530

    Changes to be committed: 	new file:   backtracking/sum_of_subset.cpp

---
+++ backtracking/sum_of_subset.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void sos(int s,int k,int r){
+    //s-> intially 0
+    //k->index of the element in the set
+    //r->intially sum of all elements 
+    
+}
